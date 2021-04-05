9355666f2c55a30be9c36d18c95b94764691d8be
matheusmoreirap852 <gota852@gmail.com>
2021-04-05 20:33:56 -0300

    aps

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -6,7 +6,7 @@ const Cabecalho = props =>{
         
         <nav  nav className="navbar fixed-top navbar-light bg-light">
         <div className="container">
-          <a className="my-0 mr-md-auto font-weight-normal">Funcionários ALL</a>
+          <a className="my-0 mr-md-auto font-weight-normal">Funcionários ALL ads</a>
             
           <a className="btn btn-outline-primary" href="#">Sair</a>
           </div>
